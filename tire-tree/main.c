97918086ff47c736f984b06f69d9284f8c35d23e
KeruWang <kw2727@nyu.edu>
2019-11-27 00:38:19 -0500

    with word & word count file

--- tire-tree/main.c
+++ tire-tree/main.c
@@ -15,6 +15,7 @@
 struct TrieNode {
     struct TrieNode *children[ALPHABET_SIZE];
     bool isEndOfWord;
+    int count;
 };
 
 // Returns new trie node (initialized to NULLs)
@@ -24,6 +25,7 @@ struct TrieNode *getNode(void) {
     if (pNode) {
         int i;
         pNode->isEndOfWord = false;
+        pNode->count = 0;
         for (i = 0; i < ALPHABET_SIZE; i++)
             pNode->children[i] = NULL;
     }
@@ -88,9 +90,25 @@ bool testTree(struct TrieNode *root, const char *key) {
     return (pCrawl != NULL && pCrawl->isEndOfWord);
 }
 
-void search(struct TrieNode *root, char *input_file_name, char *output_file_name) {
+void count(struct TrieNode *root, const char *key, int *count) {
+    int level;
+    int length = strlen(key);
+    int index;
+    struct TrieNode *pCrawl = root;
+
+    for (level = 0; level < length; level++) {
+        index = CHAR_TO_INDEX(key[level]);
+        pCrawl = pCrawl->children[index];
+    }
+
+    *count = pCrawl->count;
+    pCrawl->count = 0;
+}
+
+void search(struct TrieNode *root, char *input_file_name, char *output_file_name, char *word_file_name) {
     FILE *ifptr = fopen(input_file_name, "r");
     FILE *ofptr = fopen(output_file_name, "w");
+    FILE *wfptr = fopen(word_file_name, "w");
     char ch = fgetc(ifptr);
     int pLength = 0;
     char *buffer = (char *) malloc(sizeof(char) * 45);
@@ -112,6 +130,7 @@ void search(struct TrieNode *root, char *input_file_name, char *output_file_name
             if (pCrawl->isEndOfWord) {
                 wordLength = length;
                 isWord = true;
+                (pCrawl->count)++;
             }
             if (length < pLength) {
                 index = CHAR_TO_INDEX(buffer[length]);
@@ -127,8 +146,10 @@ void search(struct TrieNode *root, char *input_file_name, char *output_file_name
             fprintf(ofptr, "%s", " **");
             for (int i = 0; i < wordLength; i++) {
                 fprintf(ofptr, "%c", buffer[i]);
+                fprintf(wfptr, "%c", buffer[i]);
             }
             fprintf(ofptr, "%s", "** ");
+            fprintf(wfptr, "%c", ' ');
             for (int i = 0; i < 45 - wordLength; i++) {
                 buffer[i] = buffer[i + wordLength];
                 if (buffer[i]) pLength++;
@@ -144,6 +165,33 @@ void search(struct TrieNode *root, char *input_file_name, char *output_file_name
     }
     fclose(ifptr);
     fclose(ofptr);
+    fclose(wfptr);
+}
+
+void countWord(struct TrieNode *root, char *word_file_name, char *word_count) {
+    FILE *ofptr = fopen(word_file_name, "r");
+    FILE *fptr = fopen(word_count, "w");
+    if (!ofptr) assert("word_appear file not valid");
+    while (!feof(ofptr)) {
+        char ch = fgetc(ofptr);
+        char *ptr = (char *) malloc(sizeof(char));
+        int i = 0;
+        int *num = (int *) malloc(sizeof(int));
+
+        for (; !feof(ofptr) && ch != ' '; i++) {
+            ptr = (char *) realloc(ptr, sizeof(char) * (i + 1));
+            ptr[i] = ch;
+            ch = fgetc(ofptr);
+        }
+        count(root, ptr, num);
+        if (*num != 0) fprintf(fptr, "%s %d\n", ptr, *num);
+        for (int j = 0; j <= i; j++) {
+            ptr[j] = 0;
+        }
+        free(ptr);
+    }
+    fclose(ofptr);
+    fclose(fptr);
 }
 
 int main(int argc, char **argv) {
@@ -151,7 +199,8 @@ int main(int argc, char **argv) {
     // https://github.com/dolph/dictionary
     insert_file("/Users/wangkeru/Desktop/infinite-goldfish/tire-tree/dictionary_popular.txt", root);
 //    char output[][32] = {"Not present in trie", "Present in trie"};
-//    printf("%s --- %s\n", "disappear", output[testTree(root, "disappear")] );
-    search(root, "/Users/wangkeru/Desktop/infinite-goldfish/tire-tree/input.txt", "output.txt");
+//    printf("%s --- %s\n", "pled", output[testTree(root, "pled")] );
+    search(root, "/Users/wangkeru/Desktop/infinite-goldfish/tire-tree/input.txt", "output.txt", "word_appear.txt");
+    countWord(root, "word_appear.txt", "word_count.txt");
     return 0;
 }
